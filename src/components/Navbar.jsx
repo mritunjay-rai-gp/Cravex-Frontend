4b364824f36944d0f37d1c462fa214af47f4e1a6
MRITUNJAY RAI <mritunjayrai@greatpapaji-2.local>
2026-01-08 01:23:44 +0530

    navbar fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router'
-import logo from '../assets/logoC.jpg'
+
+import React, { useState } from "react";
+import { Link } from "react-router";
+import { HiMenu, HiX } from "react-icons/hi";
+import logo from "../assets/logoC.jpg";
+
 function Navbar() {
-  return (
-        <div className='navbar w-full h-20  bg-black/50 flex items-center justify-between absolute'>
-          <div className="logo flex items-center text-white align-middle gap-1 ml-10">
-            <img src={logo} alt='logo' className='rounded-2xl w-12'/>
-            <span className='font-bold text-xl'>CraveX</span>
-          </div>
-          <div className='navLinks text-white text-xl flex gap-3 items-center align-middle right-0 absolute mr-5'>
-            <Link to='/' className='hover:text-orange-300 cursor-pointer'>Home</Link>
-            <Link to='/about' className='hover:text-orange-300'>About</Link>
-            <Link to='/help' className='hover:text-orange-300'>Help</Link>
-            <Link to='/contactUs' className='hover:text-orange-300'>Contact Us</Link>
-          </div>
-        </div>   
-  )
+const [open, setOpen] = useState(false);
+
+return ( <nav className="w-full bg-black/50 absolute top-0 left-0 h-20 flex items-center justify-between px-6 text-white z-50">
+
+```
+  {/* Logo */}
+  <div className="flex items-center gap-2">
+    <img src={logo} alt="logo" className="w-12 rounded-2xl" />
+    <span className="font-bold text-xl">CraveX</span>
+  </div>
+
+  {/* Desktop Links */}
+  <div className="hidden md:flex gap-6 text-lg">
+    <Link to="/" className="hover:text-orange-300">Home</Link>
+    <Link to="/about" className="hover:text-orange-300">About</Link>
+    <Link to="/help" className="hover:text-orange-300">Help</Link>
+    <Link to="/contactUs" className="hover:text-orange-300">Contact Us</Link>
+  </div>
+
+  {/* Hamburger */}
+  <button
+    className="md:hidden text-3xl"
+    onClick={() => setOpen(!open)}
+  >
+    {open ? <HiX /> : <HiMenu />}
+  </button>
+
+  {/* Mobile Drawer */}
+  {open && (
+    <div className="absolute top-20 right-0 w-full bg-black/90 flex flex-col items-center py-4 gap-4 text-lg md:hidden">
+      <Link onClick={() => setOpen(false)} to="/" className="hover:text-orange-300">Home</Link>
+      <Link onClick={() => setOpen(false)} to="/about" className="hover:text-orange-300">About</Link>
+      <Link onClick={() => setOpen(false)} to="/help" className="hover:text-orange-300">Help</Link>
+      <Link onClick={() => setOpen(false)} to="/contactUs" className="hover:text-orange-300">Contact Us</Link>
+    </div>
+  )}
+</nav>
+
+
+);
 }
 
-export default Navbar
+export default Navbar;
